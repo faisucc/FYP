d0339c9549a1594d285228b046d1f976df58cbca
Vaisakh <realsharqi@gmail.com>
2024-02-08 21:57:00 +0530

    added similarity score computing, now need to arrange flow of control

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from googlesearch import search
 import time
 import requests
 from urllib.parse import urljoin
-
+import time
 
 
 title = ""
@@ -50,10 +50,8 @@ def getDOM(url):
    except:
       print("Failed getting DOM for url: " + url + " failed")
 
-def CalculateJaccardSimilarity(matched_domain_DOM, query_url_DOM):
-   print("need to do this function")
 
-url = "http://www.python.org"
+url = "https://www.nitc.ac.in/departments"
 domain_name = get_domain_name(url)
 baseURL = get_base_url(url)
 print("Domain name: ", domain_name)
@@ -67,11 +65,18 @@ def get_search_results(query, num_results = 10):
 
 legit_status = False
 domain_status = False
-query_url_DOM = getDOM("https://github.githubassets.com/assets/wp-runtime-f2f62e543293.js")         #this input is the URL that the user wants to visit. 
+query_url_DOM = getDOM("https://www.nitc.ac.in/")         #this input is the URL that the user wants to visit. 
 
-print(query_url_DOM)
+soup = getDOM("https://www.nitc.ac.in/departments")
+# print(soup)
+# print(query_url_DOM)
 
-# tags = soup.find_all(['hr', 'strong'])
+ 
+# img_tags = soup.find_all(['img'])
+# script_tags = soup.find_all(['script'])
+# style_tags = soup.find_all(['style'])
+# a_tags = soup.find_all(['a'])
+
 
 css_files = []
 for css in soup.find_all("link"):
@@ -79,7 +84,7 @@ for css in soup.find_all("link"):
         # if the link tag has the 'href' attribute
         css_url = urljoin(url, css.attrs.get("href"))
         css_files.append(css_url)
-print(css_files)
+# print(css_files)
 
 js_files = []
 for js in soup.find_all("script"):
@@ -87,7 +92,7 @@ for js in soup.find_all("script"):
         # if the link tag has the 'href' attribute
         js_url = urljoin(url, js.attrs.get("src"))
         js_files.append(js_url)
-print(js_files)
+# print(js_files)
 
 img_files = []
 for img in soup.find_all("img"):
@@ -95,7 +100,7 @@ for img in soup.find_all("img"):
         # if the link tag has the 'href' attribute
         img_url = urljoin(url, img.attrs.get("src"))
         img_files.append(img_url)
-print(img_files)
+# print(img_files)
 
 links_files = []
 for links in soup.find_all("a"):
@@ -103,7 +108,61 @@ for links in soup.find_all("a"):
         # if the link tag has the 'href' attribute
         links_url = urljoin(url, links.attrs.get("href"))
         links_files.append(links_url)
-print(links_files)
+# print(links_files)
+        
+
+suspicious_page_count = len(img_files) + len(links_files) + len(js_files) + len(css_files)
+print("suspicoenriugvnerjogne count = ", suspicious_page_count)
+
+
+def CalculateJaccardSimilarity(matched_domain_DOM, matched_url):
+   print("need to do this function")
+   common_elements_count = 0
+   img_tags = matched_domain_DOM.find_all(['img'])
+   script_tags = matched_domain_DOM.find_all(['script'])
+   style_tags = matched_domain_DOM.find_all(['style'])
+   a_tags = matched_domain_DOM.find_all(['a'])
+   matched_page_count = len(img_tags) + len(script_tags) + len(style_tags) + len(a_tags) 
+
+#    img_files = []
+#     for img in soup.find_all("img"):
+#         if img.attrs.get("src"):
+#             # if the link tag has the 'href' attribute
+#             img_url = urljoin(url, img.attrs.get("src"))
+#             img_files.append(img_url)
+    # print(img_files)
+
+
+   for img in img_tags:
+       if img.attrs.get("src"):
+           img_url = urljoin(matched_url,img.attrs.get("src"))
+           if img_url in img_files:
+                common_elements_count += 1
+    
+   for script in script_tags:
+        if script.attrs.get("src"):
+            script_url = urljoin(matched_url,script.attrs.get("src"))
+            if script_url in js_files:
+                common_elements_count += 1
+    
+   for style in style_tags:
+       if style.attrs.get("href"):
+           style_url = urljoin(matched_url,style.attrs.get("href"))
+           if style_url in css_files:
+               common_elements_count += 1
+
+   for a in a_tags:
+       if a.attrs.get("href"):
+           a_url = urljoin(matched_url,a.attrs.get("href"))
+           if a_url in links_files:
+                common_elements_count += 1
+
+   print("common elements = ", common_elements_count)
+   similarity_score = common_elements_count/(suspicious_page_count + matched_page_count - common_elements_count)
+   return similarity_score
+ 
+
+print(CalculateJaccardSimilarity(query_url_DOM, "https://www.nitc.ac.in/"))  #change 2nd variable to each matched result URL
 
 # if title != "Title doesnt exist":      #7
 #    query = title + ' ' + domain_name   
